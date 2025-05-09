81f8843e2dd3ffd3402a3f30218dd2041c172e31
rudkotimofei <rudko.timofei@yandex.ru>
2025-05-09 21:11:00 +0300

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "MorseAppConverter: ", log.LstdFlags)
+
+	servak := server.NewServer(logger)
+
+	logger.Println("Start server...")
+
+	err := servak.HTTPServer.ListenAndServe()
+	if err != nil {
+		logger.Fatal()
+	}
 }
